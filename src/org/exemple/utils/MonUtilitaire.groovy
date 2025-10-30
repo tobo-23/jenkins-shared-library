8e4658e0f09cf84a9ec3b4d02608054bf4047ce0
tobo-23 <nassimlad50@gmail.com>
2025-10-30 14:39:26 +0100

    Add MonUtilitaire class with utility methods

---
+++ src/org/exemple/utils/MonUtilitaire.groovy
@@ -0,0 +1,15 @@
+package org.exemple.utils
+
+class MonUtilitaire implements Serializable {
+    static void saluer(script, nom) {
+        script.echo "Bonjour de la Shared Library, ${nom} !"
+    }
+
+    static String genererMessage(String type) {
+        if (type == "succes") {
+            return "Opération réussie !"
+        } else {
+            return "Opération échouée."
+        }
+    }
+}
